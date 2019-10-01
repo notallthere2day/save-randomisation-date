073dd458132035f53b4df7589469947aad17c75f
Antony Colles <a.colles@uea.ac.uk>
2019-10-01 16:37:52 +0100

    v 1.1 added option to select level of precision for timestamp

--- save_randomization_date.php
+++ save_randomization_date.php
@@ -16,7 +16,14 @@ class save_randomization_date  extends \ExternalModules\AbstractExternalModule {
 		
 		$dateField = $this->getProjectSetting("date-field");
 		$dateEvent = $this->getProjectSetting("date-event");
+		$dateFormat = $this->getProjectSetting("date-format");
+		//set the format of the date/time
+		$dateFormats=array();
+		$dateFormats[0]="%Y-%m-%d";
+		$dateFormats[1]="%Y-%m-%d %H:%i";
+		$dateFormats[2]="%Y-%m-%d %T";
 		
+		$dateFormatString=$dateFormats[$dateFormat];
 				// Load existing value in the randomisation date field
 		$loadData = REDCap::getData('array', $record, $dateField, $dateEvent);
 		$randoDate = $loadData[$record][$dateEvent][$dateField];
@@ -26,7 +33,7 @@ class save_randomization_date  extends \ExternalModules\AbstractExternalModule {
 		if($randoDate =="") {
 						
 			// Get the randomisation date from the log
-			$sql = "SELECT DATE(ts) AS 'logged_date'
+			$sql = "SELECT DATE_FORMAT(ts, '$dateFormatString') AS 'logged_date'
 			FROM redcap_log_event
 			WHERE description = 'randomize record' AND pk = '" . $record . "' AND project_id = " . $project_id;
 
